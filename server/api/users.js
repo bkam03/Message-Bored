02d0b359be3a2dc8a635fca574a8f34128d7c307
BK <bkam03@yahoo.com>
2017-09-16 12:09:13 -1000

    post, get, getOne works

--- server/api/users.js
+++ server/api/users.js
@@ -1,9 +1,43 @@
 const express = require( 'express' );
 
+const db = require( '../models');
+const User = db.user;
+
 const router = express.Router();
 
-router.get( '/', ( req, res ) => {
+router.get('/:id', ( req, res ) => {
+  console.log( `/api/users/id = ${ req.params.id }`);
+  User.findOne( {
+    where: {
+      id: req.params.id
+    }
+  } )
+  .then( ( user ) => {
+    console.log( 'find one user', user );
+    res.send( user );
+  } )
+  .catch( ( err ) => {
+    console.log( err );
+  } );
+} );
+
+router.route( '/' )
+.get( ( req, res ) => {
   console.log( '/api/users GET' );
+  User.findAll()
+  .then( ( users ) => {
+    console.log( 'users', users );
+    res.send( users );
+  } )
+  .catch( ( err ) => {
+    console.log( err );
+  } );
+} )
+.post( ( req, res ) => {
+  console.log( '/api/users POST' );
+  User.create( {
+    name: req.body.name
+  } );
   res.end();
 } );
 
