5be4fb7a87fb88ac20b43f86283403328c49f709
BK <bkam03@yahoo.com>
2017-09-16 11:10:40 -1000

    server set up

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require( 'express' );
+const bp = require( 'body-parser' );
 
-const PORT = process.env.PORT || 3000;
+const db = require( './models' );
 
+const PORT = process.env.PORT || 3000
 const app = express();
+
+app.use( bp.json( { extended: true } ) );
+
+app.use( express.static( 'public' ) );
 
 const server = app.listen( PORT, () => {
   console.log( `server listening at port ${ PORT }`);
+  db.sequelize.sync( { force: true } );
 } );
