efee1840bf94303fd8b02e1221c69ac4160fbfe2
BK <bkam03@yahoo.com>
2017-09-16 15:55:41 -1000

    added app.get * to make index appear

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require( 'express' );
 const bp = require( 'body-parser' );
+const path = require( 'path' );
 
 const db = require( './models' );
 const apiRoute = require( './api/index.js' );
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use( bp.json( { extended: true } ) );
@@ -12,6 +13,10 @@ app.use( bp.json( { extended: true } ) );
 app.use( express.static( 'public' ) );
 
 app.use( '/api', apiRoute );
+
+app.get( '*', ( req, res ) => {
+  res.sendFile( 'index.html', { root: path.join(__dirname, '../public') } );
+} );
 
 const server = app.listen( PORT, () => {
   console.log( `server listening at port ${ PORT }`);
