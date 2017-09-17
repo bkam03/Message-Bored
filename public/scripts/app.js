4d503e9495eccd40d1a817573c50d3168c76400b
BK <bkam03@yahoo.com>
2017-09-16 18:58:41 -1000

    enabled multiview, Url was misspelt

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,19 @@
+
+
 angular.module( 'app', [ 'ngRoute' ] );
 
+
 angular.module( 'app' )
-  .config( [ '$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ){
-    $routeProvider
-    .when( '/', {
-      templateUrl: 'home.html',
-      controller: 'homeController'
-    } )
-    .otherwise( {
-      template: '<h1><center>you have failed</center></h1>'
-    } );
-    $locationProvider.html5Mode( true );
-  } ] )
-  .run([]);
+.config( [ '$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ){
+  $routeProvider
+  .when( '/', {
+    templateUrl: 'home.html',
+    controller: 'homeController'
+  } )
+  .otherwise( {
+    template: '<h1><center>Nope</center></h1>'
+  } );
+
+  $locationProvider.html5Mode( true );
+} ] )
+.run( function(){} );
