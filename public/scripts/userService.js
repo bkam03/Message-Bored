fb23195bca531988c64e36532b802657bf34f24d
BK <bkam03@yahoo.com>
2017-09-17 10:33:13 -1000

    service now get users

--- public/scripts/userService.js
+++ public/scripts/userService.js
@@ -1,12 +1,33 @@
 angular.module( 'app' )
-.service( 'usersService', [ '$http', function( $http ){
+.service( 'UserService', [ '$http', function( $http ){
 
-  var getUsers = function(){
+  /*var getUsers = function(){
     return $http.get( '/users' )
     .then( ( users ) => {
       console.log( users );
       return users.data;
     } );
+
+  };*/
+  var users = [];
+
+/*  $http.get( '/api/users' )
+  .then( ( users ) => {
+    console.log( 'user service', users );
+    users = users;
+  } );
+*/
+  function getUsers() {
+    //$http
+    return $http.get('/api/users')
+    .then((users) => {
+      console.log('users', users );
+      return users.data;
+    });
+  }
+
+  return {
+    getUsers: getUsers
 
   };
 
