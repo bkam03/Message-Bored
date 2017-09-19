6a66da2f3a5cc76ae1523583ea6726dc5b37168f
BK <bkam03@yahoo.com>
2017-09-18 16:29:35 -1000

    showUserHandler now uses a promise, properly sends info to view

--- public/scripts/UserController.js
+++ public/scripts/UserController.js
@@ -5,11 +5,27 @@ angular.module( 'app' )
 
   UserService.getUsers()
   .then( ( users ) => {
-    console.log( 'users in controller', users );
     $scope.users = users;
+  } )
+  .catch( ( err ) => {
+    console.log( err );
   } );
 
-  $scope.getUser = UserService.getUser;
 
-  $scope.myModel="default";
+
+  function showUserHandler( data ){
+    let userId = data.user.id;
+    UserService.getUser( userId )
+    .then( ( user ) => {
+      $scope.user = user;
+    } )
+    .catch( ( err ) => {
+      console.log( err );
+    } );
+  }
+
+  $scope.showUserHandler = showUserHandler;
+
+/*  $scope.getUser = UserService.getUser;
+*/
 } ] );
