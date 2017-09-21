a5eb3f8eaeeb49f1d361b374232baf3da65954c7
BK <bkam03@yahoo.com>
2017-09-20 20:20:40 -1000

    added test console logss

--- server/api/users.js
+++ server/api/users.js
@@ -3,13 +3,14 @@ const express = require( 'express' );
 const { User } = require( '../models');
 const router = express.Router();
 
-router.get('/:id', ( req, res ) => {
+router.get('/:name', ( req, res ) => {
   User.findOne( {
     where: {
-      id: req.params.id
+      name: req.params.name
     }
   } )
   .then( ( user ) => {
+    console.log( 'in route, db returned', user );
     res.send( user );
   } )
   .catch( ( err ) => {
