3ced4dcabb1c9828dcb41fd60c57eb74634432f6
BK <bkam03@yahoo.com>
2017-09-20 21:28:38 -1000

    now updates its menu items every second

--- public/scripts/navigationController.js
+++ public/scripts/navigationController.js
@@ -1,5 +1,20 @@
 angular.module( 'app' )
-.controller( 'navigationController', [ '$scope', function( $scope ){
+.controller( 'navigationController', [ '$scope', '$rootScope', function( $scope, $rootScope ){
 
-  $scope.isSignedIn = false;
+  function logout(){
+
+  }
+
+  $scope.isSignedIn = true;
+
+  setInterval( function(){
+    console.log( 'interval', $rootScope.isSignedIn, $scope.isSignedIn );
+    if( $scope.isSignedIn !== $rootScope.isSignedIn ){
+      console.log( 'inside if' );
+      $scope.isSignedIn = $rootScope.isSignedIn;
+      $scope.$apply();
+    }
+  }, 1000 );
+
+
 } ] );
