3e4655f000de04ddbfde76615a709ce25c9d9745
BK <bkam03@yahoo.com>
2017-09-20 20:19:19 -1000

    adds user if found in db to localStorage

--- public/scripts/loginController.js
+++ public/scripts/loginController.js
@@ -1,8 +1,28 @@
 angular.module( 'app' )
-.controller( 'loginController', [ '$scope', 'signInOutService', function( $scope, signInOutService ){
-  $scope.test = 'no';
-  $scope.addUserToLocalStorage = signInOutService.addUserToLocalStorage;
-  $scope.removeUserFromLocalStorage = signInOutService.removeUserFromLocalStorage;
-  $scope.getUserFromLocalStorage = signInOutService.getUserFromLocalStorage;
+.controller( 'loginController', [ '$scope', 'localStorageService', 'UserService', function( $scope, localStorageService, UserService ){
+  $scope.error = '@';
+
+  function signInUser( username ){
+    console.log( 'firing signInUser in controller', username );
+    $scope.error = "";
+    UserService.getUser( username )
+    .then( ( user ) => {
+      if( user ){
+        let { id, name } = user;
+        console.log( 'found user', id, name );
+        localStorageService.addUserToLocalStorage( id, name );
+      } else {
+        $scope.error = "username not found";
+      }
+    } )
+    .catch( ( err ) => {
+      $scope.error = err;
+    } );
+  }
+
+
+  $scope.signInUser = signInUser;
+  $scope.removeUserFromLocalStorage = localStorageService.removeUserFromLocalStorage;
+  $scope.getUserFromLocalStorage = localStorageService.getUserFromLocalStorage;
 
 } ] );
