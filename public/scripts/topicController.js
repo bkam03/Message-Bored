2e241fc998977b5d8375e8393f966ac50fb169cc
BK <bkam03@yahoo.com>
2017-09-21 20:06:27 -1000

    connected function to view, will handle post message once complete

--- public/scripts/topicController.js
+++ public/scripts/topicController.js
@@ -1,6 +1,19 @@
 angular.module( 'app' )
-.controller( 'topicController', [ '$scope', '$routeParams', function( $scope, $routeParams ){
-  $scope.topic = $routeParams.id;
-  console.log( $routeParams );
+.controller( 'topicController', [ '$scope', '$routeParams', 'TopicService', function( $scope, $routeParams, TopicService ){
+
+
+  /*
+    query topic by id
+    put topic up on view
+    query message table for those from
+  */
+
+  function handleMessagePost( message ){
+    console.log( message );
+  }
+
+  $scope.handleMessagePost = handleMessagePost;
+
+  $scope.message = "";
 
 } ] );
