5f4abaebf23d0be501b77f3f4cb16f75a4a49296
BK <bkam03@yahoo.com>
2017-09-21 19:10:14 -1000

    route works properly / GETusers

--- server/api/topics.js
+++ server/api/topics.js
@@ -33,18 +33,17 @@ router.route( '/' )
 
 } )
 .post( ( req, res ) => {
-  console.log( 'post route received', req.body );
   let data = req.body;
   Topic.create( {
     topic: data.topic,
     created_by: data.userId
   } )
   .then( ( topic ) => {
-    console.log( 'posted to db', topic );
     res.send( topic );
   } )
   .catch( ( err ) => {
     console.log( err );
+    res.send( err );
   } );
 } );
 
