ade49eb08d7f133b0f9e774af8efd7251556b739
BK <bkam03@yahoo.com>
2017-09-21 19:09:46 -1000

    removed console logs

--- public/scripts/newtopicController.js
+++ public/scripts/newtopicController.js
@@ -4,13 +4,11 @@ angular.module( 'app' )
 
   function submitNewTopic( newTopic ){
     let userId = localStorageService.getUserIdFromLocalStorage();
-    console.log( 'new topic', newTopic, userId );
     TopicService.addTopic( newTopic, userId )
     .then( ( user ) => {
-      console.log( 'controller received ', user );
     } )
     .catch( ( err ) => {
-      console.log( err );
+      $scope.error = err;
     } );
 
   }
