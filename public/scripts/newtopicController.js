d250a14d20ea4a82badd18d0249c1436f0d111f1
BK <bkam03@yahoo.com>
2017-09-21 18:27:36 -1000

    calls localStorage for userId, connected topicService

--- public/scripts/newtopicController.js
+++ public/scripts/newtopicController.js
@@ -1,9 +1,12 @@
 
 angular.module( 'app' )
-.controller( 'newtopicController', [ '$scope', function( $scope ){
+.controller( 'newtopicController', [ '$scope', 'TopicService','localStorageService', function( $scope, TopicService, localStorageService ){
 
   function submitNewTopic( newTopic ){
-    console.log( 'new topic', newTopic );
+    let userId = localStorageService.getUserIdFromLocalStorage();
+    console.log( 'new topic', newTopic, userId );
+    TopicService.addTopic( newTopic, userId );
+
   }
 
   $scope.newtopic= "";
