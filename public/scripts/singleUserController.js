70b115336264fd436d0ec70a0bf103e173cd5379
BK <bkam03@yahoo.com>
2017-09-23 10:33:36 -1000

    sets scope for user and message list

--- public/scripts/singleUserController.js
+++ public/scripts/singleUserController.js
@@ -1,6 +1,14 @@
 angular.module( 'app' )
-.controller( 'singleUserController', [ '$scope', 'UserService', function( $scope, UserService ){
+.controller( 'singleUserController', [ '$scope', '$routeParams', 'UserService', function( $scope, $routeParams, UserService ){
 
-
+  UserService.getUserById( $routeParams.user_id )
+  .then( ( user ) => {
+    console.log( 'returned to controller', user );
+    $scope.user = user;
+    $scope.messages = user.Messages;
+  } )
+  .catch( ( err ) => {
+    console.log( err );
+  } );
 
 } ] );
