5efbe13de83c89403cc17c6e004366b848c5ace3
BK <bkam03@yahoo.com>
2017-09-23 15:41:37 -1000

    changes rootscope, if signed in?

--- public/scripts/registerController.js
+++ public/scripts/registerController.js
@@ -1,5 +1,5 @@
 angular.module( 'app' )
-.controller( 'registerController', [ '$scope', 'UserService', function( $scope, UserService ){
+.controller( 'registerController', [ '$scope', '$location', 'UserService', 'localStorageService', function( $scope, $location, UserService, localStorageService ){
   $scope.UserService = UserService;
 
   $scope.username = "";
@@ -8,6 +8,9 @@ angular.module( 'app' )
   function addUser( username ){
     UserService.addUser( username )
     .then( ( newUser ) => {
+      console.log( newUser );
+      localStorageService.addUserToLocalStorage( newUser.id, newUser.name );
+      $location.path( '/' );
     } )
     .catch( ( err ) => {
       console.log( err );
