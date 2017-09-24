bf92418bfc171cd754c109ab1c93b2c25f9e9a81
BK <bkam03@yahoo.com>
2017-09-23 19:35:57 -1000

    requests all messages, and shuffles them before handing it off to scope

--- public/scripts/homeController.js
+++ public/scripts/homeController.js
@@ -1,4 +1,31 @@
 angular.module( 'app' )
-.controller( 'homeController', [ '$scope', function( $scope ){
+.controller( 'homeController', [ '$scope', 'MessageService', function( $scope, MessageService ){
+
+  //durstenfield shuffle, borrowed.
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+  }
+
+  $scope.messages=[];
+
+  MessageService.getAllMessages()
+  .then( ( messages ) => {
+    let messageArray = [];
+    for( var key in messages ){
+      messageArray.push( messages[ key ] );
+    }
+    console.log( 'return to controller', messageArray );
+    $scope.messages = shuffleArray( messageArray );
+    console.log( 'shuffled', $scope.messages );
+  } )
+  .catch( ( err ) => {
+    console.log( err );
+  } );
 
 } ] );
